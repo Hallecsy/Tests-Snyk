19688c5c65e8ba4cd2afc72665093fc09fe4159a
Hallecsy <alexismuguetpro@gmail.com>
2023-06-02 12:16:13 +0200

    Resolve conflict

---
+++ script.js
@@ -0,0 +1,5 @@
+let titleElement = document.getElementById("title");
+let name = new URLSearchParams(window.location.search).get("name");
+if (name !== null) {
+  titleElement.innerHTML = `Hello name`;
+}
